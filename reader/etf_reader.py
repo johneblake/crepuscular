f6f3efc7643c46109502995e202abcf7ba7772e6
John Blake <johneblake@hotmail.com>
2017-07-23 18:37:13 -0700

    Get etf_reader working

--- reader/etf_reader.py
+++ reader/etf_reader.py
@@ -1,1 +1,11 @@
-http://www.nasdaq.com/investing/etfs/etf-finder-results.aspx?download=Yes
+import csv
+import requests
+
+def get_etf():
+    """Download csv from nasdaq of etf"""
+    download = requests.get("http://www.nasdaq.com/investing/etfs/etf-finder-results.aspx?download=Yes")
+    decoded_content = download.content.decode(download.encoding)
+
+    cr = csv.reader(decoded_content.splitlines())
+    for line in cr:
+        print("Symbol = {symbol}, Description = {description}".format(symbol=line[0], description=line[1]))
