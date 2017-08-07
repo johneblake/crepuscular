e70fcd8fec5b7b28b8c83ff1b2021b976c1de640
John Blake <johneblake@hotmail.com>
2017-08-07 17:57:45 -0600

    stub out main

--- main.py
+++ main.py
@@ -5,10 +5,30 @@ from reader import quandl_reader, etf_reader
 if len(sys.argv) > 1:
     command = sys.argv[1]
     if command == "help":
-        print("historical, daily, help")
+        print("create, historical, daily, help")
+    elif command == "create":
+        create()
     elif command == "historical":
-        pass
+        historical()
     elif command == "daily":
-        pass
+        daily()
     else:
         print("Unknown command {command}".format(command = command))
+
+def create():
+    # grab etf symbols
+    # grab the historical zip file from quandl
+    # delete existing database
+    # create new database
+    # load symbols
+    pass
+
+def historical():
+    # load historical data
+    # grab the etf symbols and get historical data
+    pass
+
+def daily():
+    # get etf symbols and grab the daily data based on last run
+    # get daily quandl using the diff file
+    pass
